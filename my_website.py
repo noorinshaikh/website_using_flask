ae92f90148a2bd0a570dce6f830c91c6126f8450
Noorin Shaikh <noorinshaikh08@gmail.com>
2019-11-20 18:16:41 -0800

    Added forest fire tables html Added forest fires map created in Project2
    Signed-off-by: Noorin Shaikh <noorinshaikh08@gmail.com>

--- my_website.py
+++ my_website.py
@@ -2,6 +2,15 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+
+@app.route('/forest_fires_table/')
+def forest_fires_table():
+  return render_template("forest_fires_table.html")
+
+@app.route('/Forest_fires/')
+def Forest_fires():
+  return render_template("Forest_fires.html")
+
 @app.route('/description/')
 def description():
   return render_template("description.html")
